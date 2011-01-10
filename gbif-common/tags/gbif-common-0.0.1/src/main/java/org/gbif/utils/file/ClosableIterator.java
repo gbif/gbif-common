41c00287d823c8c58c8b16cf120ce49b59e59e88
wixner <wixner@0ccb6880-a3f6-2ae5-22e0-ac1ed81bfb6c>
2011-01-10 23:00:00 +0000

    [maven-release-plugin]  copy for tag gbif-common-0.0.1

---
+++ gbif-common/tags/gbif-common-0.0.1/src/main/java/org/gbif/utils/file/ClosableIterator.java
@@ -0,0 +1,12 @@
+package org.gbif.utils.file;
+
+import java.util.Iterator;
+
+/** An iterator that needs to be explicitly closed when it is not used anymore
+ * @author markus
+ *
+ * @param <T>
+ */
+public interface ClosableIterator<T> extends Iterator<T>{
+	public void close();
+}
