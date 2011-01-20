70f1b7ef635e8f8842284718a13c01df615ed122
wixner <wixner@0ccb6880-a3f6-2ae5-22e0-ac1ed81bfb6c>
2011-01-20 16:54:25 +0000

    [maven-release-plugin]  copy for tag gbif-common-0.0.2

---
+++ gbif-common/tags/gbif-common-0.0.2/src/main/java/org/gbif/utils/collection/MapUtils.java
@@ -0,0 +1,28 @@
+package org.gbif.utils.collection;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class MapUtils {
+
+	static public <K, V> Map<K, V> sortByValue(Map<K, V> map) {
+	     List list = new LinkedList(map.entrySet());
+	     Collections.sort(list, new Comparator() {
+	          public int compare(Object o1, Object o2) {
+	               return ((Comparable) ((Map.Entry) (o1)).getValue())
+	              .compareTo(((Map.Entry) (o2)).getValue());
+	          }
+	     });
+		Map result = new LinkedHashMap<K, V>();
+		for (Iterator it = list.iterator(); it.hasNext();) {
+		     Map.Entry entry = (Map.Entry)it.next();
+		     result.put(entry.getKey(), entry.getValue());
+		     }
+		return result;
+	}
+}
