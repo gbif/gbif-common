4d9cefc74b0374214d17fe6e64fa09a52b43792c
lars.francke <lars.francke@0ccb6880-a3f6-2ae5-22e0-ac1ed81bfb6c>
2011-02-11 15:48:12 +0000

    Convert test to Java5 syntax as well.

--- gbif-common/trunk/src/test/java/org/gbif/utils/collection/MapUtilsTest.java
+++ gbif-common/trunk/src/test/java/org/gbif/utils/collection/MapUtilsTest.java
@@ -1,18 +1,20 @@
 package org.gbif.utils.collection;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
+
+import static org.junit.Assert.assertEquals;
 
 public class MapUtilsTest {
 
+
   @Test
   public void testMap() {
-    HashMap<String, Double> map = new HashMap<String, Double>();
+    Map<String, Double> map = new HashMap<String, Double>(4);
     map.put("A", 99.5);
     map.put("B", 67.4);
     map.put("C", 67.5);
@@ -22,7 +24,7 @@ public class MapUtilsTest {
 
     assertEquals(4, mapSorted.size());
 
-    LinkedList<String> expected = new LinkedList<String>();
+    Queue<String> expected = new LinkedList<String>();
     expected.add("D");
     expected.add("B");
     expected.add("C");
@@ -34,17 +36,17 @@ public class MapUtilsTest {
 
   @Test
   public void testMap2() {
-    HashMap map = new HashMap();
+    Map<Integer, String> map = new HashMap<Integer, String>(4);
     map.put(5, "C");
     map.put(4, "B");
     map.put(99, "A");
     map.put(3, "D");
 
-    Map<Integer, Double> mapSorted = MapUtils.sortByValue(map);
+    Map<Integer, String> mapSorted = MapUtils.sortByValue(map);
 
     assertEquals(4, mapSorted.size());
 
-    LinkedList<Integer> expected = new LinkedList<Integer>();
+    Queue<Integer> expected = new LinkedList<Integer>();
     expected.add(99);
     expected.add(4);
     expected.add(5);
