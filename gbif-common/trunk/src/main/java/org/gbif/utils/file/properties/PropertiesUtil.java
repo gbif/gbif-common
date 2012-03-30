2e4ac29a06dd2e59f25bf25df1d547999ecc5af1
federicomh@gmail.com <federicomh@gmail.com@0ccb6880-a3f6-2ae5-22e0-ac1ed81bfb6c>
2012-03-30 08:00:24 +0000

    Utility class for handling properties files.

---
+++ gbif-common/trunk/src/main/java/org/gbif/utils/file/properties/PropertiesUtil.java
@@ -0,0 +1,41 @@
+package org.gbif.utils.file.properties;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Properties;
+
+import com.google.common.io.Closeables;
+import com.google.common.io.Resources;
+
+/**
+ * Utility class for handling properties files.
+ */
+public class PropertiesUtil {
+
+
+  /**
+   * Loads a properties file.
+   * The file should be available in the classpath, the default {@link ClassLoader} is used to load the file.
+   */
+  public static Properties loadProperties(String propertiesFile) throws IOException {
+    InputStream inputStream = null;
+    Properties tempProperties;
+    try {
+      URL configFileURL = Resources.getResource(propertiesFile);
+      inputStream = Resources.newInputStreamSupplier(configFileURL).getInput();
+      tempProperties = new Properties();
+      tempProperties.load(inputStream);
+    } finally {
+      Closeables.closeQuietly(inputStream);
+    }
+    return tempProperties;
+  }
+
+  /**
+   * Private default constructor.
+   */
+  private PropertiesUtil() {
+    // empty block
+  }
+}
