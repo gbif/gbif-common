b0216bf9de2a3e9d05a7d37b8ae6f1d90872caf0
wixner <wixner@0ccb6880-a3f6-2ae5-22e0-ac1ed81bfb6c>
2012-06-21 13:54:49 +0000

    Make SolrModule configurable, but add default solr synonyms, stopwords, etc files

--- gbif-common/trunk/src/main/java/org/gbif/utils/file/ResourcesUtil.java
+++ gbif-common/trunk/src/main/java/org/gbif/utils/file/ResourcesUtil.java
@@ -9,11 +9,15 @@ import java.net.URL;
 import com.google.common.io.Closeables;
 import com.google.common.io.Files;
 import com.google.common.io.Resources;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Utils class dealing with classpath resources in addition to guavas {@link Resources}.
  */
 public class ResourcesUtil {
+
+  private static final Logger LOG = LoggerFactory.getLogger(ResourcesUtil.class);
 
   /**
    * Static utils class.
@@ -25,17 +29,28 @@ public class ResourcesUtil {
   /**
    * Copies classpath resources to real files.
    *
-   * @param folder             to copy resource files into
-   * @param classpathPrefix    common prefix added to all classpath resources which is not used for the result file
-   *                           path.
-   * @param classpathResources list of classpath resources to be copied into folder
+   * @param folder                 to copy resource files into
+   * @param ignoreMissingResources if true ignores missing resources, throws IOException otherwise
+   * @param classpathPrefix        common prefix added to all classpath resources which is not used for the result file
+   *                               path.
+   * @param classpathResources     list of classpath resources to be copied into folder
    */
-  public static void copy(File folder, String classpathPrefix, String... classpathResources) throws IOException {
+  public static void copy(File folder, String classpathPrefix, boolean ignoreMissingResources,
+    String... classpathResources) throws IOException {
     for (String classpathResource : classpathResources) {
       String res = classpathPrefix + classpathResource;
-      URL url = Resources.getResource(res);
-      if (url == null) {
-        throw new IOException("Classpath resource " + res + " not existing");
+      URL url = null;
+      try {
+        url = Resources.getResource(res);
+        if (url == null) {
+          throw new IllegalArgumentException("Classpath resource " + res + " not existing");
+        }
+      } catch (IllegalArgumentException e) {
+        if (ignoreMissingResources) {
+          LOG.debug("Resource {} not found", res);
+          continue;
+        }
+        throw new IOException(e);
       }
       File f = new File(folder, classpathResource);
       Files.createParentDirs(f);
