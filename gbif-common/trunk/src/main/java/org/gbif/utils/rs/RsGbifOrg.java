91f525904a74be3b641a14f7e5720598c98ec995
wixner <wixner@0ccb6880-a3f6-2ae5-22e0-ac1ed81bfb6c>
2012-07-10 08:51:17 +0000

    Adding tests and a new url method for cleaner implementation

--- gbif-common/trunk/src/main/java/org/gbif/utils/rs/RsGbifOrg.java
+++ gbif-common/trunk/src/main/java/org/gbif/utils/rs/RsGbifOrg.java
@@ -3,6 +3,7 @@ package org.gbif.utils.rs;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import com.google.common.base.Joiner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,8 +11,9 @@ import org.slf4j.LoggerFactory;
  * Utility class knowing the url layout of rs.gbif.org to access authority and dictionary files.
  */
 public class RsGbifOrg {
-  protected static final Logger log = LoggerFactory.getLogger(RsGbifOrg.class);
-  public static final String DICTIONARY_URL = "http://rs.gbif.org/dictionaries/";
+  private static final Logger LOG = LoggerFactory.getLogger(RsGbifOrg.class);
+  private static final Joiner PATH_JOINER = Joiner.on("/").skipNulls();
+  public static final String DOMAIN = "http://rs.gbif.org/";
   public static final String FILENAME_BLACKLIST = "blacklisted.txt";
   public static final String FILENAME_SUPRAGENERIC = "suprageneric.txt";
   public static final String FILENAME_EPITHETA = "epitheta.txt";
@@ -21,24 +23,35 @@ public class RsGbifOrg {
   public static final String FILENAME_GENERA_AMIGOUS = "genera_ambigous.txt";
 
   /**
+   * @param path given as array of individual names that will be concatenated
+   * @return url to file inside rs.gbif.org
+   */
+  public static URL url(String ... path) {
+    try {
+      if (path == null){
+        return new URL(DOMAIN);
+      }
+      return new URL(DOMAIN + PATH_JOINER.join(path));
+    } catch (MalformedURLException e) {
+      LOG.error("Cannot create rs.gbif.org url for path " + PATH_JOINER.join(path), e);
+    }
+    return null;
+  }
+
+  /**
+  * @param filename of dictionary file requested
+  * @return url to file inside to dictionary folder of rs.gbif.org
+  */
+  public static URL dictionaryUrl(String filename) {
+    return url("dictionaries", filename);
+  }
+
+  /**
    * @param filename of authority dictionary file requested
    * @return url to file inside to authority folder of rs.gbif.org
    */
   public static URL authorityUrl(String filename) {
-    return dictionaryUrl("authority/" + filename);
-  }
-
-  /**
-   * @param filename of dictionary file requested
-   * @return url to file inside to dictionary folder of rs.gbif.org
-   */
-  public static URL dictionaryUrl(String filename) {
-    try {
-      return new URL(DICTIONARY_URL + "/" + filename);
-    } catch (MalformedURLException e) {
-      log.error("Cannot create dictionary url for file " + filename, e);
-    }
-    return null;
+    return url("dictionaries", "authority", filename);
   }
 
   /**
@@ -46,6 +59,6 @@ public class RsGbifOrg {
    * @return url to file inside to synonyms dictionary folder of rs.gbif.org
    */
   public static URL synonymUrl(String filename) {
-    return dictionaryUrl("synonyms/" + filename);
+    return url("dictionaries", "synonyms", filename);
   }
 }
