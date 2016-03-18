95438dd7f00c37cfbd54db148a82e9ddb8d95737
Christian Gendreau <cgendreau@gbif.org>
2016-03-18 13:54:09 +0100

    comments

--- src/test/java/org/gbif/utils/number/BigDecimalUtilsTest.java
+++ src/test/java/org/gbif/utils/number/BigDecimalUtilsTest.java
@@ -14,7 +14,10 @@ import static org.junit.Assert.assertFalse;
  */
 public class BigDecimalUtilsTest {
 
-
+  /**
+   * This test is simply to illustrate why we use the BigDecimal(String) constructor.
+   * new BigDecimal(23.1) equals 23.10000000000000142108547152020037174224853515625
+   */
   @Test
   public void testBigDecimalConstructor(){
     assertFalse(new BigDecimal(23.1d).equals(new BigDecimal("23.1")));
@@ -27,8 +30,6 @@ public class BigDecimalUtilsTest {
     assertEquals(new BigDecimal("23.0"), BigDecimalUtils.fromDouble(23.0, false));
     assertEquals(new BigDecimal("23"), BigDecimalUtils.fromDouble(23.0000000000, true));
     assertEquals(new BigDecimal("23.000000000001"), BigDecimalUtils.fromDouble(23.000000000001, true));
-
-
   }
 
 }
